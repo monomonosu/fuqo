6bbf6cc5576ee6a506882391b555d12e251cd468
monomonosu <pojijyan@yahoo.co.jp>
2025-11-04 04:13:30 +0900

    QueryEnginが見つからない旨のエラー解消

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/.prisma/client/**/*"],
+    "/*": ["./node_modules/.prisma/client/**/*"],
   },
 };
 
